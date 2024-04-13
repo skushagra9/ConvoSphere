309a7eedd9c5d5f9e03431d4d6d99b3289849162
Kushagra <skushagra.sharma@gmail.com>
2024-04-13 13:22:22 +0530

    converted to monorepo

---
+++ packages/prisma/prisma/migrations/20240412094420_new/migration.sql
@@ -0,0 +1,63 @@
+/*
+  Warnings:
+
+  - The primary key for the `Account` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `id` on the `Account` table. All the data in the column will be lost.
+  - The primary key for the `Session` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `id` on the `Session` table. All the data in the column will be lost.
+  - Added the required column `updatedAt` to the `Account` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Session` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `email` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropIndex
+DROP INDEX "Account_provider_providerAccountId_key";
+
+-- DropIndex
+DROP INDEX "VerificationToken_identifier_token_key";
+
+-- DropIndex
+DROP INDEX "VerificationToken_token_key";
+
+-- AlterTable
+ALTER TABLE "Account" DROP CONSTRAINT "Account_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ADD CONSTRAINT "Account_pkey" PRIMARY KEY ("provider", "providerAccountId");
+
+-- AlterTable
+ALTER TABLE "Session" DROP CONSTRAINT "Session_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ALTER COLUMN "email" SET NOT NULL;
+
+-- AlterTable
+ALTER TABLE "VerificationToken" ADD CONSTRAINT "VerificationToken_pkey" PRIMARY KEY ("identifier", "token");
+
+-- CreateTable
+CREATE TABLE "Authenticator" (
+    "id" TEXT NOT NULL,
+    "credentialID" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "credentialPublicKey" TEXT NOT NULL,
+    "counter" INTEGER NOT NULL,
+    "credentialDeviceType" TEXT NOT NULL,
+    "credentialBackedUp" BOOLEAN NOT NULL,
+    "transports" TEXT,
+
+    CONSTRAINT "Authenticator_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Authenticator_credentialID_key" ON "Authenticator"("credentialID");
+
+-- AddForeignKey
+ALTER TABLE "Authenticator" ADD CONSTRAINT "Authenticator_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
